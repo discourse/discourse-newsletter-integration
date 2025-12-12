4c8e40088e3dc7d26166027ee38f45ed58be1105
zogstrip <regis@hanol.fr>
2025-12-12 17:50:52 +0100

    DEV: Use preferences-save-attributes transformer for newsletter field
    https://github.com/discourse/discourse/36658 introduced a `preferences-save-attributes`
    value transformer, changing `saveAttrNames` from a mutable property
    to a getter. This broke the previous approach of pushing to the array
    in the controller's init method.
    
    Update to use the new transformer API with backward compatibility
    for older Discourse versions that don't have this transformer.

--- assets/javascripts/discourse/initializers/setup.js
+++ assets/javascripts/discourse/initializers/setup.js
@@ -1,4 +1,5 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
+import { VALUE_TRANSFORMERS } from "discourse/lib/transformer/registry";
 
 export default {
   name: "discourse-newsletter-integration-setup",
@@ -14,16 +15,29 @@ export default {
         "newsletter_integration_subscribe_global_newsletter"
       );
 
-      api.modifyClass("controller:preferences/emails", {
-        pluginId: "discourse-newsletter-integration-emails-preference",
+      if (VALUE_TRANSFORMERS.includes("preferences-save-attributes")) {
+        api.registerValueTransformer(
+          "preferences-save-attributes",
+          ({ value: attrs, context }) => {
+            if (context.page === "emails") {
+              attrs.push("newsletter_integration_subscribe_global_newsletter");
+            }
+            return attrs;
+          }
+        );
+      } else {
+        // Backward compatibility for older Discourse versions
+        api.modifyClass("controller:preferences/emails", {
+          pluginId: "discourse-newsletter-integration-emails-preference",
 
-        init() {
-          this._super(...arguments);
-          this.saveAttrNames.push(
-            "newsletter_integration_subscribe_global_newsletter"
-          );
-        },
-      });
+          init() {
+            this._super(...arguments);
+            this.saveAttrNames.push(
+              "newsletter_integration_subscribe_global_newsletter"
+            );
+          },
+        });
+      }
     });
   },
 };
