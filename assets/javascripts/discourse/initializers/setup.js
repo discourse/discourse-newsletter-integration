86a69d721e6f7389778eb4e5a481a48454884f0e
zogstrip <regis@hanol.fr>
2025-12-17 19:22:58 +0100

    DEV: Use new addSaveableUserField plugin API
    Simplify preference saving by using the new
    `addSaveableUserField(name, { page: "emails" })` API.
    
    This replaces manual value transformer registration and backward
    compatibility code with a single declarative API call.
    
    cf. https://github.com/discourse/discourse/pull/36757

--- assets/javascripts/discourse/initializers/setup.js
+++ assets/javascripts/discourse/initializers/setup.js
@@ -1,43 +1,18 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { VALUE_TRANSFORMERS } from "discourse/lib/transformer/registry";
 
 export default {
   name: "discourse-newsletter-integration-setup",
 
   initialize() {
     withPluginApi((api) => {
-      const currentUser = api.getCurrentUser();
-      if (!currentUser) {
+      if (!api.getCurrentUser()) {
         return;
       }
 
       api.addSaveableUserField(
-        "newsletter_integration_subscribe_global_newsletter"
+        "newsletter_integration_subscribe_global_newsletter",
+        { page: "emails" }
       );
-
-      if (VALUE_TRANSFORMERS.includes("preferences-save-attributes")) {
-        api.registerValueTransformer(
-          "preferences-save-attributes",
-          ({ value: attrs, context }) => {
-            if (context.page === "emails") {
-              attrs.push("newsletter_integration_subscribe_global_newsletter");
-            }
-            return attrs;
-          }
-        );
-      } else {
-        // Backward compatibility for older Discourse versions
-        api.modifyClass("controller:preferences/emails", {
-          pluginId: "discourse-newsletter-integration-emails-preference",
-
-          init() {
-            this._super(...arguments);
-            this.saveAttrNames.push(
-              "newsletter_integration_subscribe_global_newsletter"
-            );
-          },
-        });
-      }
     });
   },
 };
